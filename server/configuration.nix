011f9cc68d2a048d409587e1adcf20d22fa7ca76
totto2727 <kaihatu.totto2727@gmail.com>
2024-04-03 00:15:08 +0900

    feat: disable auto suspend

--- server/configuration.nix
+++ server/configuration.nix
@@ -46,14 +46,16 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  # services.xserver.displayManager.sddm.enable = true;
+  # services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.autoSuspend = false;
 
   # Configure keymap in X11
   services.xserver = {
     layout = "us";
     xkbVariant = "";
-    displayManager.gdm.autoSuspend = false;
   };
 
   # Enable CUPS to print documents.
