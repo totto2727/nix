55e522fa8f3ad873ca05dd691045f4aa2129b705
totto2727 <kaihatu.totto2727@gmail.com>
2024-04-06 19:18:13 +0900

    feat: add dev shell for setup

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -13,10 +13,31 @@
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+
+      # Systems supported
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper to provide system-specific attributes
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
     in {
       homeConfigurations."totto2727" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         modules = [ ./home.nix ];
       };
+      
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            go-task
+          ];
+        };
+      });
     };
 }
