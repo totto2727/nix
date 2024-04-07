26d9d05002e0d0453cb08bfb7c303e2c6ceb4b15
totto2727 <kaihatu.totto2727@gmail.com>
2024-04-08 01:11:57 +0900

    feat: add setup server

---
+++ setup/server/README.md
@@ -0,0 +1,28 @@
+# nix
+
+## install
+
+- desktop: gnome
+- timezome: Asia/Tokyo
+- keyboard layout: US
+- language: English(US)
+- user: any
+
+## initialize
+
+```bash
+curl https://raw.githubusercontent.com/totto2727/nix/main/setup/server/configuration.nix | sudo tee /etc/nixos/configuration.nix
+sudo nixos-rebuild switch
+
+curl https://raw.githubusercontent.com/totto2727/nix/main/setup/server/sky-barium1/flake.nix | sudo tee /etc/nixos/flake.nix
+sudo nixos-rebuild switch
+```
+
+## update
+
+```bash
+curl https://raw.githubusercontent.com/totto2727/nix/main/setup/serverconfiguration.nix | sudo tee /etc/nixos/configuration.nix
+curl https://raw.githubusercontent.com/totto2727/nix/main/setup/serverflake.nix | sudo tee /etc/nixos/flake.nix
+
+sudo nixos-rebuild switch
+```
---
+++ setup/server/configuration.nix
@@ -0,0 +1,103 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ 
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Enable firewalld
+  networking.firewall.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Tokyo";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "ja_JP.UTF-8";
+    LC_IDENTIFICATION = "ja_JP.UTF-8";
+    LC_MEASUREMENT = "ja_JP.UTF-8";
+    LC_MONETARY = "ja_JP.UTF-8";
+    LC_NAME = "ja_JP.UTF-8";
+    LC_NUMERIC = "ja_JP.UTF-8";
+    LC_PAPER = "ja_JP.UTF-8";
+    LC_TELEPHONE = "ja_JP.UTF-8";
+    LC_TIME = "ja_JP.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.autoSuspend = false;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Configure sshd
+  services.openssh = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      X11Forwarding = true;
+      PermitRootLogin = "no"; # disable root login
+      PasswordAuthentication = false; # disable password login
+    };
+  };
+
+  system.stateVersion = "23.11";
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  networking.hostName = "sky-barium1";
+
+  users.mutableUsers = false;
+  users.users.totto2727 = {
+    hashedPassword = "$6$Fv4iD1gjAeYSmwRi$albtFuthGyIu6Ze8wMtVVbnJo0t7oMFXUNho.LEXMp8HloxcRZE8syNNSkx2mBl1JXlHv0blNsQ8fC73J6kJE.";
+    isNormalUser = true;
+    description = "totto2727";
+    extraGroups = [ "networkmanager" "wheel" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFZIpTFFowBxrPTOS+2qsc0lvn38Yzk1MbZpw18ohvaV"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHvtQSg0rgO51SueoWR40NLN6mWpaAPcIupmctYabhXW"
+    ];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+  ];
+
+  virtualisation = {
+    docker = {
+      enable = true;
+      rootless = {
+        enable = true;
+        setSocketVariable = true; # $DOCKER_HOSTを設定
+      };
+    };
+  };
+
+  services.github-runners = {
+    nix-sky-barium1 = {
+      enable = true;
+      name = "nix-sky-barium1";
+      url = "https://github.com/totto2727/nix";
+      tokenFile = "/secrets/token1";
+    };
+  }
+}
