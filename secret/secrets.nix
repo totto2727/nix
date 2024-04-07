400eebc2f102caf50795120a677d6b3fd3bee734
totto2727 <kaihatu.totto2727@gmail.com>
2024-04-08 01:30:23 +0900

    feat: add github-token.age

---
+++ secret/secrets.nix
@@ -0,0 +1,5 @@
+let
+  sky-barium1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMyJf5avsEozQ7f/V5jQsGqiZ8jv+waLDCZRzOQmn193";
+in
+{
+}
---
+++ server/sky-barium1/secret/secrets.nix
@@ -0,0 +1,6 @@
+let
+  sky-barium1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMyJf5avsEozQ7f/V5jQsGqiZ8jv+waLDCZRzOQmn193";
+in
+{
+  "github-token.age".publicKeys = [ sky-barium1 ];
+}
