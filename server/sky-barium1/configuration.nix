fea42e982a97d71f8eac0d019afed938a4039caf
totto2727 <kaihatu.totto2727@gmail.com>
2024-04-24 22:33:22 +0900

    feat: enforce disable suspend

--- server/sky-barium1/configuration.nix
+++ server/sky-barium1/configuration.nix
@@ -40,6 +40,19 @@
   services.xserver.desktopManager.gnome.enable = true;
   services.xserver.displayManager.gdm.autoSuspend = false;
 
+  programs.dconf.enable = true;
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+        if (action.id == "org.freedesktop.login1.suspend" ||
+            action.id == "org.freedesktop.login1.suspend-multiple-sessions" ||
+            action.id == "org.freedesktop.login1.hibernate" ||
+            action.id == "org.freedesktop.login1.hibernate-multiple-sessions")
+        {
+            return polkit.Result.NO;
+        }
+    });
+  '';
+
   # Configure keymap in X11
   services.xserver = {
     layout = "us";
